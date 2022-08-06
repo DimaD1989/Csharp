85a11e27f594c1c6a66d5c81637a0f2d0da5cc1d
DimaD1989 <d_d1989sv@mail.ru>
2022-08-06 21:48:53 +0300

    Задача_2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 )
+    Console.Write("First number is a max");
+else
+    Console.Write("Second number is max");
+
