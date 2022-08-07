ea9362c35bde4c01fd54f2edc06f5bef904b59af
DimaD1989 <d_d1989sv@mail.ru>
2022-08-07 12:17:16 +0300

    Задача6

--- Program.cs
+++ Program.cs
@@ -23,3 +23,15 @@
 //if (number3>max) max= number3;
 //Console.WriteLine(" max number " + max );
 
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Input first number:");
+int number1=Convert.ToInt32(Console.ReadLine()); //4
+if  ((double)number1%2==0)
+Console.Write(" Number1 - Четное");
+else
+Console.Write(" Number - Нечетное");
